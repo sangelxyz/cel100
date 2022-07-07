2ef2cc6797898fc7c53e899da116fd907382bf05
sangel <msgs@sangel.xyz>
2022-07-07 12:08:20 +0700

    made changes

--- pages/_document.js
+++ pages/_document.js
@@ -12,10 +12,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400&display=swap" rel="stylesheet" />
-<<<<<<< HEAD
         <link href="../styles/style.css" rel="stylesheet" />
-=======
->>>>>>> bdc42c285e8fc0ee43f1d78480eb545f0d3958c4
         </Head>
       <body>
         <Main />
